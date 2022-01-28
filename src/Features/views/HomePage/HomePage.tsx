b2bd0f633d5eaf90a7bb9053aba0fcb47e874b51
nfcsportal <nfcsdev@gmail.com>
2022-01-28 22:52:04 +0400

    feat:ts configs, react routing, modules, construction

---
+++ src/Features/views/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './HomePage.module.scss';
+
+const HomePage: React.FC = () => {
+  return <div className={styles.main}>Home Page</div>;
+};
+
+export default HomePage;
