55e4e422e274782a8cd7288ea07ae4587808a380
nfcsportal <98554478+nfcsportal@users.noreply.github.com>
2022-01-28 22:53:27 +0400

    Preprod (#13)
    * test
    
    * feat:add yaml
    
    * feat:add yaml2
    
    * yaml2
    
    * feat:ts configs, react routing, modules, construction (#12)

---
+++ src/Features/views/AboutUs/AboutUs.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './AboutUs.module.scss';
+
+const AboutUs: React.FC = () => {
+  return <div className={styles.main}>AboutUs</div>;
+};
+
+export default AboutUs;
