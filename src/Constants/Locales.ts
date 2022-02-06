85947142fc469a0d73aa10e1152f4abb769d42bc
nfcsportal <nfcsdev@gmail.com>
2022-02-06 13:32:58 +0400

    feat: update deps, usemediaquery, header/footer

--- src/Constants/Locales.ts
+++ src/Constants/Locales.ts
@@ -1,8 +1,9 @@
 import german from '../../src/Assets/Locales/de.json';
 import english from '../../src/Assets/Locales/en.json';
 import french from '../../src/Assets/Locales/fr.json';
+import { JSONObject } from '../Types/global';
 
-export const LOCALES: any = {
+export const LOCALES: { [key: string]: JSONObject } = {
   ['en']: english,
   ['de']: german,
   ['fr']: french,
