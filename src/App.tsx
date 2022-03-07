e61fa7b62437029e458c2a4681e91dd4043b84a1
nfcsportal <nfcsdev@gmail.com>
2022-03-08 00:30:59 +0400

    feat: coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 import { IntlProvider } from 'react-intl';
 import { useSelector } from 'react-redux';
 
+import logo from '../src/Assets/images/logo.svg';
 import { LOCALES } from './Constants/Locales';
-import Routes from './routes';
+// import Routes from './routes';
 import { getCurrentLocale } from './Store/Selectors/main';
 
 const App: React.FC = () => {
   const currentLocale = useSelector(getCurrentLocale);
+  console.log(logo);
 
   return (
     <>
       <IntlProvider locale={currentLocale} messages={LOCALES[currentLocale]}>
-        <Routes />
+        <div className="soon">
+          <div className="logo">
+            <img src={logo} />
+          </div>
+          <div className="comingSoon">Coming Soon ...</div>
+        </div>
+        {/* <Routes /> */}
       </IntlProvider>
     </>
   );
