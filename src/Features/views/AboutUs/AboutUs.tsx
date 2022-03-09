8c61527417e4e83f92b72ba387ed326d5f94a15d
unknown <narek.avagyan.tw@gmail.com>
2022-03-09 23:47:17 +0400

    feature/about-contact-pages: part of contact and about

--- src/Features/views/AboutUs/AboutUs.tsx
+++ src/Features/views/AboutUs/AboutUs.tsx
@@ -1,9 +1,114 @@
 import React from 'react';
 
+import EnvelopeSvg from '../../../Assets/Icons/EnvelopeSvg';
+import LocationSvg from '../../../Assets/Icons/LocationSvg';
+import PhoneSvg from '../../../Assets/Icons/PhoneSvg';
+import TelegramSvg from '../../../Assets/Icons/TelegramSvg';
+import WhatsAppSvg from '../../../Assets/Icons/WhatsAppSvg';
+import Footer from '../../moleculs/Footer';
+import Header from '../../moleculs/Header';
+import MainContent from '../../organisms/MainContent';
 import styles from './AboutUs.module.scss';
 
 const AboutUs: React.FC = () => {
-  return <div className={styles.main}>AboutUs</div>;
+  return (
+    <div className="about-us-page page">
+      <Header />
+      <MainContent>
+        <section className={styles.aboutUsSection}>
+          <div className="container">
+            <div className={styles.aboutUsRow}>
+              <div className={`${styles.col_left} col_50`}>
+                <div className={styles.aboutUsInfo}>
+                  <h1 className="title">About Us</h1>
+                  <p className="subTitle">
+                    Our culture reflects our philosophy of respect, simplify, and solve. Success is
+                    a result of our talented and dedicated team who live and exemplify our
+                    philosophy. The NFCS team expects great things of each other, treat each other
+                    well, and are committed to creating the best analytic in the world.
+                  </p>
+                  <div className={styles.aboutUsContactContainer}>
+                    <div className={styles.aboutUsContact}>
+                      <p className={styles.contactItem}>
+                        <span>
+                          <LocationSvg />
+                        </span>
+                        Yerevan, Buzand 34/2
+                      </p>
+                      <a href="tel:+37455846474" className={styles.contactItem}>
+                        <span>
+                          <PhoneSvg />
+                        </span>
+                        +374 55 846474
+                      </a>
+                    </div>
+                    <ul className={styles.aboutSocialList}>
+                      <li>
+                        <span>
+                          <TelegramSvg />
+                        </span>
+                      </li>
+                      <li>
+                        <span>
+                          <WhatsAppSvg />
+                        </span>
+                      </li>
+                      <li>
+                        <span>
+                          <EnvelopeSvg />
+                        </span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className={`${styles.col_right} col_50`}>
+                <div className={styles.timelineContainer}>
+                  <ul>
+                    <li>
+                      <div className={styles.timelineContent}>
+                        <h3 className={styles.timelineTitle}>V1 VAULTS:</h3>
+                        <p className={styles.timelineText}>
+                          More global scope analytics for Futures,Bond analytics Fund analytics.
+                        </p>
+                      </div>
+                      <div className={styles.timelineDate}>
+                        <p>Janruary 12</p>
+                      </div>
+                    </li>
+                    <li>
+                      <div className={styles.timelineContent}>
+                        <h3 className={styles.timelineTitle}>V1 VAULTS:</h3>
+                        <p className={styles.timelineText}>
+                          More global scope analytics for Futures,Bond analytics Fund analytics.
+                        </p>
+                      </div>
+                      <div className={styles.timelineDate}>
+                        <p>Janruary 12</p>
+                      </div>
+                    </li>
+                    <li>
+                      <div className={styles.timelineContent}>
+                        <h3 className={styles.timelineTitle}>V1 VAULTS:</h3>
+                        <p className={styles.timelineText}>
+                          More global scope analytics for Futures,Bond analytics Fund analytics.
+                        </p>
+                      </div>
+                      <div className={styles.timelineDate}>
+                        <p>Janruary 12</p>
+                      </div>
+                    </li>
+                    <div className={styles.bothClenar} />
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </MainContent>
+      <Footer />
+    </div>
+  );
 };
 
 export default AboutUs;
---
+++ src/Features/views/Contact/Contact.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Footer from '../../moleculs/Footer';
+import Header from '../../moleculs/Header';
+import MainContent from '../../organisms/MainContent';
+import styles from './Contact.module.scss';
+
+const Contact = () => {
+  return (
+    <div className="page contact-page">
+      <Header />
+      <MainContent>
+        <section className={styles.contactSection}>
+          <div className="container">
+            <div className={styles.contactRow}>
+              <div className={`${styles.col_left} col_item`}>
+                <div className={styles.contactInfo}>
+                  <h1 className="title">Contact Us</h1>
+                  <p className="subTitle">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aenean turpis et a
+                    quis. In varius tristique ornare adipiscing mattis elit nisi enim. Et lorem
+                    pellentesque volutpat elit id mauris est aenean ut. Diam tincidunt commodo
+                    quisque mi.
+                  </p>
+                </div>
+              </div>
+              <div className={`${styles.col_right} col_item`}></div>
+            </div>
+          </div>
+        </section>
+      </MainContent>
+      <Footer />
+    </div>
+  );
+};
+
+export default Contact;
