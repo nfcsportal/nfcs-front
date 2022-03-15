da48d917df468734318a5c142c1219100094c26a
unknown <narek.avagyan.tw@gmail.com>
2022-03-15 21:42:33 +0400

    feature/about-page-responsive: part of about resp, and loader styles

--- src/Constants/ScreenResolutions.ts
+++ src/Constants/ScreenResolutions.ts
@@ -1,6 +1,8 @@
 export const SCREENS: { [key: string]: string } = {
   mobile: '(max-width: 576px)',
+  smallTablet: '(max-width: 767px)',
   tablet: '(max-width: 1024px)',
+  onlyTablet: '(max-width: 1280px) and (min-width: 768px)',
   bigTablet: '(max-width: 1280px)',
   desktop: '(max-width: 1440px)',
   largeDesktop: '(min-width: 1440px)',
--- src/Features/views/AboutUs/AboutUs.tsx
+++ src/Features/views/AboutUs/AboutUs.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import useMediaQuery from 'react-use-media-query-hook';
 
 import EnvelopeSvg from '../../../Assets/Icons/EnvelopeSvg';
 import LocationSvg from '../../../Assets/Icons/LocationSvg';
 import PhoneSvg from '../../../Assets/Icons/PhoneSvg';
 import TelegramSvg from '../../../Assets/Icons/TelegramSvg';
 import WhatsAppSvg from '../../../Assets/Icons/WhatsAppSvg';
+import { SCREENS } from '../../../Constants/ScreenResolutions';
 import Footer from '../../moleculs/Footer';
 import Header from '../../moleculs/Header';
 import MainContent from '../../organisms/MainContent';
 import styles from './AboutUs.module.scss';
 
 const AboutUs: React.FC = () => {
+  const isOnlyTablet = useMediaQuery(SCREENS.onlyTablet);
   return (
     <div className={`${styles.aboutPage} about-us-page page`}>
       <Header />
@@ -63,44 +66,101 @@ const AboutUs: React.FC = () => {
                 </div>
               </div>
               <div className={`${styles.col_right} col_50`}>
-                <div className={styles.timelineContainer}>
-                  <ul>
-                    <li>
-                      <div className={styles.timelineContent}>
-                        <h3 className={styles.timelineTitle}>V1 VAULTS:</h3>
-                        <p className={styles.timelineText}>
-                          More global scope analytics for Futures,Bond analytics Fund analytics.
-                        </p>
+                <>
+                  {!isOnlyTablet ? (
+                    <div className={styles.timelineContainer}>
+                      <ul>
+                        <li>
+                          <div className={styles.timelineContent}>
+                            <h3 className={styles.timelineTitle}>V1 VAULTS:</h3>
+                            <p className={styles.timelineText}>
+                              More global scope analytics for Futures,Bond analytics Fund analytics.
+                            </p>
+                          </div>
+                          <div className={styles.timelineDate}>
+                            <p>Janruary 12</p>
+                          </div>
+                        </li>
+                        <li>
+                          <div className={styles.timelineContent}>
+                            <h3 className={styles.timelineTitle}>V1 VAULTS:</h3>
+                            <p className={styles.timelineText}>
+                              More global scope analytics for Futures,Bond analytics Fund analytics.
+                            </p>
+                          </div>
+                          <div className={styles.timelineDate}>
+                            <p>Janruary 12</p>
+                          </div>
+                        </li>
+                        <li>
+                          <div className={styles.timelineContent}>
+                            <h3 className={styles.timelineTitle}>V1 VAULTS:</h3>
+                            <p className={styles.timelineText}>
+                              More global scope analytics for Futures,Bond analytics Fund analytics.
+                            </p>
+                          </div>
+                          <div className={styles.timelineDate}>
+                            <p>Janruary 12</p>
+                          </div>
+                        </li>
+                        <div className={styles.bothClenar} />
+                      </ul>
+                    </div>
+                  ) : (
+                    <div className={styles.tabletTimeline}>
+                      <div className={styles.tabletItemsTop}>
+                        <div className={styles.tabletItemsRow}>
+                          <div className={`${styles.tabletItem} col_33`}>
+                            <div className={styles.tableItemBlock}>
+                              <h2 className={styles.tableItemBlockTitle}>V1 VAULTS:</h2>
+                              <p className={styles.tableItemBlockText}>
+                                More global scope analytics for Futures,Bond analytics Fund
+                                analytics.
+                              </p>
+                            </div>
+                          </div>
+                          <div className={`${styles.tabletItem} col_33`}>
+                            <div className={styles.tableItemBlock}>
+                              <h2 className={styles.tableItemBlockTitle}>V1 VAULTS:</h2>
+                              <p className={styles.tableItemBlockText}>
+                                More global scope analytics for Futures,Bond analytics Fund
+                                analytics.
+                              </p>
+                            </div>
+                          </div>
+                          <div className={`${styles.tabletItem} col_33`}>
+                            <div className={styles.tableItemBlock}>
+                              <h2 className={styles.tableItemBlockTitle}>V1 VAULTS:</h2>
+                              <p className={styles.tableItemBlockText}>
+                                More global scope analytics for Futures,Bond analytics Fund
+                                analytics.
+                              </p>
+                            </div>
+                          </div>
+                        </div>
                       </div>
-                      <div className={styles.timelineDate}>
-                        <p>Janruary 12</p>
+                      <div className={styles.tabletItemsBottom}>
+                        <div className={styles.tabletItemsRow}>
+                          <div className={`${styles.tabletItem} col_33`}>
+                            <div className={styles.tableItemBlock}>
+                              <p className={styles.tableItemBlockText}>sep 2022</p>
+                            </div>
+                          </div>
+                          <div className={`${styles.tabletItem} col_33`}>
+                            <div className={styles.tableItemBlock}>
+                              <p className={styles.tableItemBlockText}>Jan 2023</p>
+                            </div>
+                          </div>
+                          <div className={`${styles.tabletItem} col_33`}>
+                            <div className={styles.tableItemBlock}>
+                              <p className={styles.tableItemBlockText}>jun 2023</p>
+                            </div>
+                          </div>
+                        </div>
                       </div>
-                    </li>
-                    <li>
-                      <div className={styles.timelineContent}>
-                        <h3 className={styles.timelineTitle}>V1 VAULTS:</h3>
-                        <p className={styles.timelineText}>
-                          More global scope analytics for Futures,Bond analytics Fund analytics.
-                        </p>
-                      </div>
-                      <div className={styles.timelineDate}>
-                        <p>Janruary 12</p>
-                      </div>
-                    </li>
-                    <li>
-                      <div className={styles.timelineContent}>
-                        <h3 className={styles.timelineTitle}>V1 VAULTS:</h3>
-                        <p className={styles.timelineText}>
-                          More global scope analytics for Futures,Bond analytics Fund analytics.
-                        </p>
-                      </div>
-                      <div className={styles.timelineDate}>
-                        <p>Janruary 12</p>
-                      </div>
-                    </li>
-                    <div className={styles.bothClenar} />
-                  </ul>
-                </div>
+                    </div>
+                  )}
+                </>
               </div>
             </div>
           </div>
