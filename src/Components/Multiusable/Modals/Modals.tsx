b2a55f3e7fe7977066efa642c949841f0b48e8ce
nfcsportal <98554478+nfcsportal@users.noreply.github.com>
2022-03-16 14:19:36 -0700

    Feat: implement framer motion (#58)

--- src/Components/Multiusable/Modals/Modals.tsx
+++ src/Components/Multiusable/Modals/Modals.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
 import Modal from 'react-modal';
 import { useDispatch } from 'react-redux';
@@ -10,6 +11,28 @@ import Typography from '../../../Features/atoms/Typography';
 import { useCurrentButtonActions, useModal } from '../../../Hooks/useModal';
 import { setErrorMessage, setModal } from '../../../Store/Slices/modal';
 import styles from './modal.module.scss';
+
+const badSuspension = {
+  hidden: {
+    y: '-100vh',
+    opacity: 0,
+    transform: 'scale(0) rotateX(-360deg)',
+  },
+  visible: {
+    y: '0',
+    opacity: 1,
+    transition: {
+      duration: 0.2,
+      type: 'spring',
+      damping: 35,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: '-100vh',
+    opacity: 0,
+  },
+};
 
 Modal.setAppElement('#root');
 
@@ -36,28 +59,41 @@ const Modals: React.FC = () => {
       overlayClassName={styles.modalOverlay}
       className={styles.modalClass}
     >
-      <div className={styles.modalInner}>
-        {currentModal?.outsideClose && (
-          <div onClick={closeModal} className={styles.modalCloser}>
-            <CloseSvg />
-          </div>
-        )}
-        <div className={styles.modalBody}>
-          <Typography component="h2" id={currentModal?.title} className={styles.modalTitle} />
-          <Typography component="p" id={currentModal?.subtitle} className={styles.modalSubTitle} />
-          <div className={styles.modalIcon}>
-            {currentModal?.type === 'error' ? <ErrorSvg /> : <SuccesSvg />}
-          </div>
-          <div className={styles.modalBtn}>
-            <Button
-              onClick={currentModalButtonAction}
-              type="primary"
-              customClass={styles.cardBtn}
-              id={currentModal?.buttonText}
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          key="modal"
+          className={styles.modalInner}
+          variants={badSuspension}
+          initial="hidden"
+          exit="initial"
+          animate="visible"
+        >
+          {currentModal?.outsideClose && (
+            <div onClick={closeModal} className={styles.modalCloser}>
+              <CloseSvg />
+            </div>
+          )}
+          <div className={styles.modalBody}>
+            <Typography component="h2" id={currentModal?.title} className={styles.modalTitle} />
+            <Typography
+              component="p"
+              id={currentModal?.subtitle}
+              className={styles.modalSubTitle}
             />
+            <div className={styles.modalIcon}>
+              {currentModal?.type === 'error' ? <ErrorSvg /> : <SuccesSvg />}
+            </div>
+            <div className={styles.modalBtn}>
+              <Button
+                onClick={currentModalButtonAction}
+                type="primary"
+                customClass={styles.cardBtn}
+                id={currentModal?.buttonText}
+              />
+            </div>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </AnimatePresence>
     </Modal>
   );
 };
