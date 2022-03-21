c9c3bea73a5020b28b331060a1ca0bb5d9cc9d27
nfcsportal <nfcsdev@gmail.com>
2022-03-21 23:47:47 +0400

    Feat:proxy

--- src/Service/api/intercepter.ts
+++ src/Service/api/intercepter.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 export const API_URL =
-  'https://cors-anywhere.herokuapp.com/http://ec2-18-233-167-133.compute-1.amazonaws.com:8080/api/health';
+  'https://cors-anywhere.herokuapp.com/http://ec2-18-233-167-133.compute-1.amazonaws.com:8080/api';
 const $api = axios.create({
   withCredentials: true,
   baseURL: API_URL,
@@ -11,7 +11,7 @@ $api.interceptors.request.use((config: AxiosRequestConfig) => {
   const tokent = localStorage.getItem('token');
   if (config && config.headers) {
     config.headers.Authorization = `Bearer ${tokent}`;
-    config.headers.origin = 'origin';
+    config.headers['Access-Control-Allow-Headers'] = 'x-requested-with, x-requested-by';
     return config;
   }
 });
