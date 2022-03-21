818a8dc54439072cf7174d877ec400724fdadae1
nfcsportal <nfcsdev@gmail.com>
2022-03-21 21:13:12 +0400

    feat: add icons

--- src/Constants/descriptions.ts
+++ src/Constants/descriptions.ts
@@ -1,18 +1,27 @@
+import React from 'react';
+
+import CryptoBigSvg from '../Assets/Icons/CryptoBigSvg';
+
 export type TDescriptions = {
   current: string;
+  icon: React.FC;
 };
 
 export const DESCRIPTIONS: TDescriptions[] = [
   {
     current: 'stock',
+    icon: CryptoBigSvg,
   },
   {
     current: 'nft',
+    icon: CryptoBigSvg,
   },
   {
     current: 'futures',
+    icon: CryptoBigSvg,
   },
   {
     current: 'crypto',
+    icon: CryptoBigSvg,
   },
 ];
