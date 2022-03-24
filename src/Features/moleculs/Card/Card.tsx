f07c9aba8e91b7e6269a9b5178515be07e633a7a
nfcsportal <nfcsdev@gmail.com>
2022-03-25 01:28:22 +0400

    Feat: create infra

--- src/Features/moleculs/Card/Card.tsx
+++ src/Features/moleculs/Card/Card.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import TooltipSvg from '../../../Assets/Icons/cards/TooltipSvg';
 import Button from '../../atoms/Button';
+import ToolTip from '../../atoms/ToolTip';
 import Typography from '../../atoms/Typography';
 import { THomeCards } from '../../organisms/organismsTypes';
 import styles from './card.module.scss';
@@ -21,25 +22,28 @@ const Card: React.FC<ICard> = ({
   hoverEffect,
 }) => {
   const Component = icon;
+
+  const [toolTip, setToolTip] = useState<boolean>(false);
   return (
     <div className={`${styles.cardsCol_25} col_25`}>
-      <div
-        className={`${styles.toltipContent} ${styles.toltipContentSmall} ${styles.toltipContentNormal} ${styles.toltipContentBig}`}
-      >
-        <div className={styles.tooltipArrow} />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae et arcu cras dolor
-          sollicitudin viverra maecenas pretium faucibus. Fermentum diam magnis suscipit dui
-          malesuada ut aenean vel. Nec a, adipiscing tortor a feugiat nec sit.
-        </p>
+      <div>
+        <ToolTip
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae et arcu cras dolor
+        sollicitudin viverra maecenas pretium faucibus. Fermentu"
+          isOpen={toolTip}
+        />
       </div>
       <div className={`${styles.cardItem} ${styles[`${hoverEffect}`]}`}>
         <div className={styles.cardTooltip}>
-          <button type="button" aria-label="tooltip">
+          <button
+            onMouseLeave={() => setToolTip(false)}
+            onMouseEnter={() => setToolTip(true)}
+            type="button"
+            aria-label="tooltip"
+          >
             <TooltipSvg />
           </button>
         </div>
-
         <div className={styles.cardIcon}>
           <Component />
         </div>
